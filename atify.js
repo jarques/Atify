f5560fd5338305f37cb20dd5d2ebd6efede04758
jarquesp <hello@jpretorius.com>
2009-10-17 02:47:22 -0700

    check if html is null

--- atify.js
+++ atify.js
@@ -11,11 +11,13 @@
 $.fn.atify = function(customOptions){
 	var options = $.extend({},$.fn.atify.defaultOptions, customOptions);
 	var html = $(this).html();
-	html = html.replace(/@([A-Za-z0-9_]+)/gi,"@<a href=\"http://twitter.com/$1\">$1</a>");
-	if(options.hashtag){
-	    html = html.replace(/#([A-Za-z0-9_-]+)/gi,"<a href=\"http://twitter.com/search?q=%22$1%22\">#$1</a>");
+	if (html !== null) {
+		html = html.replace(/@([A-Za-z0-9_]+)/gi,"@<a href=\"http://twitter.com/$1\">$1</a>");
+		if(options.hashtag){
+		    html = html.replace(/#([A-Za-z0-9_-]+)/gi,"<a href=\"http://twitter.com/search?q=%22$1%22\">#$1</a>");
+		}
+		$(this).html(html);
 	}
-	$(this).html(html);
 }
 	
 $.fn.atify.defaultOptions = {
