7a7967532f817a2eaac58d17fe96adbdc956633c
Ryan LeFevre <meltingice8917@gmail.com>
2009-10-17 03:14:15 -0400

    Removed console.log()... again. Don't judge me.

--- atify.js
+++ atify.js
@@ -13,7 +13,6 @@ $.fn.atify = function(customOptions){
 	var html = $(this).html();
 	html = html.replace(/@([A-Za-z0-9_]+)/gi,"@<a href=\"http://twitter.com/$1\">$1</a>");
 	if(options.hashtag){
-	    console.log("running hashtag");
 	    html = html.replace(/#([A-Za-z0-9_-]+)/gi,"<a href=\"http://twitter.com/#search?q=%22$1%22\">#$1</a>");
 	}
 	$(this).html(html);
