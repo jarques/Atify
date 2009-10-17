59d87c340e1196240a6e01d7ad06e1557886e9a1
jarquesp <hello@jpretorius.com>
2009-10-17 02:56:11 -0700

    updated compressed atify script

--- atify.c.js
+++ atify.c.js
@@ -1,3 +1,3 @@
 /*Atify v1 by @jarques & @meltingice*/
-$.fn.atify = function(customOptions){var options = $.extend({},$.fn.atify.defaultOptions, customOptions);var html = $(this).html();html = html.replace(/@([A-Za-z0-9_]+)/gi,"@<a href=\"http://twitter.com/$1\">$1</a>");if(options.hashtag){html = html.replace(/#([A-Za-z0-9_-]+)/gi,"<a href=\"http://twitter.com/#search?q=%22$1%22\">#$1</a>");}$(this).html(html);}
+$.fn.atify = function(customOptions){var options = $.extend({},$.fn.atify.defaultOptions, customOptions);var html = $(this).html();if (html) {html = html.replace(/@([A-Za-z0-9_]+)/gi,"@<a href=\"http://twitter.com/$1\">$1</a>");if(options.hashtag){html = html.replace(/#([A-Za-z0-9_-]+)/gi,"<a href=\"http://twitter.com/search?q=%22$1%22\">#$1</a>");}$(this).html(html);}}
 $.fn.atify.defaultOptions = {'hashtag':false}
