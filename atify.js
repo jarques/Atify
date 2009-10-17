b112454bb7736d7e240b2c8bb2208b25307ccca6
jarquesp <hello@jpretorius.com>
2009-10-16 22:26:27 -0700

    long version of script

--- atify.js
+++ atify.js
@@ -3,5 +3,9 @@
 *	Follow me @jarques - http://twitter.com/@jarques
 */
 
-$.fn.atify = function(){var html = $(this).html();html = html.replace(/@([^\s@]+)/gi,"<a href=\"http://twitter.com/$1\">@$1</a>");$(this).html(html);}
+$.fn.atify = function(){
+	var html = $(this).html();
+	html = html.replace(/@([^\s@]+)/gi,"<a href=\"http://twitter.com/$1\">@$1</a>");
+	$(this).html(html);
+}
 
