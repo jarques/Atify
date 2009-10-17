39e0298ee5709f49123aa8a2f69231cb3197dfdd
Ryan LeFevre <meltingice8917@gmail.com>
2009-10-17 03:11:23 -0400

    Modified regex to be more accurate, moved @ symbol outside of the username link, and added hashtag info to the README file

--- atify.js
+++ atify.js
@@ -11,9 +11,10 @@
 $.fn.atify = function(customOptions){
 	var options = $.extend({},$.fn.atify.defaultOptions, customOptions);
 	var html = $(this).html();
-	html = html.replace(/@([^\s@]+)/gi,"<a href=\"http://twitter.com/$1\">@$1</a>");
+	html = html.replace(/@([A-Za-z0-9_]+)/gi,"@<a href=\"http://twitter.com/$1\">$1</a>");
 	if(options.hashtag){
-		html = html.replace(/#([^\s#]+)/gi,"<a href=\"http://twitter.com/#search?q=%22$1%22\">#$1</a>");
+	    console.log("running hashtag");
+	    html = html.replace(/#([A-Za-z0-9_-]+)/gi,"<a href=\"http://twitter.com/#search?q=%22$1%22\">#$1</a>");
 	}
 	$(this).html(html);
 }
